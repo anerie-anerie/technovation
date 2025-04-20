cedf6c0e963b4d8fcbc0bccdad65afca929e3c34
Maggie <mwu082008@gmail.com>
2025-04-19 20:23:45 -0400

    heroku

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
